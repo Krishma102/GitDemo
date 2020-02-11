301b7d8ddfd13bfd8250eec814e7e850cf8fd3ba
krishma <kapatel0204@gmail.com>
2020-02-11 14:51:27 -0500

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test // first add testng library // still maven don't know without dependcny
+	public void BrowserAutomation()
+	{
+		System.out.println(" BrowserAutomation");
+	}
+
+	//@Test
+	public void elemnetsUI()
+	{
+		System.out.println(" elemnetsUI");
+	}
+}
